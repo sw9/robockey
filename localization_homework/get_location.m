5bef3028198bcedfddd1f2660ef82486dba7f065
sw9 <sw9@outlook.com>
2017-01-06 22:04:32 -0500

    added code to make plots for localization homework

--- localization_homework/get_location.m
+++ localization_homework/get_location.m
@@ -1,15 +1,14 @@
 function [loc, rot] = get_location(arr)
 
 max_d = -1;
-f = 1280;
 offset = [512 384];
 
 for i=1:3
     for j=i+1:4
-        i_x = arr((i-1)*3 + 1);
-        i_y = arr((i-1)*3 + 2);
-        j_x = arr((j-1)*3 + 1);
-        j_y = arr((j-1)*3 + 2);
+        i_x = arr((i-1)*2 + 1);
+        i_y = arr((i-1)*2 + 2);
+        j_x = arr((j-1)*2 + 1);
+        j_y = arr((j-1)*2 + 2);
         d = sqrt((i_x - j_x)^2 + (i_y - j_y)^2);
         if d > max_d
             ind_i = i;
@@ -22,16 +21,13 @@ for i=1:3
 end
 
 center = [(p_i(1)+p_j(1))/2 (p_i(2)+p_j(2))/2];
-center - offset
-loc = -(center - offset)/f*5;
 
 i_sum = 0;
 j_sum = 0;
-
 for k=1:4
     if k ~= ind_i && k~= ind_j
-        k_x = arr((k-1)*3 + 1);
-        k_y = arr((k-1)*3 + 2);
+        k_x = arr((k-1)*2 + 1);
+        k_y = arr((k-1)*2 + 2);
         d_i = sqrt((k_x - p_i(1))^2 + (k_y - p_i(2))^2);
         d_j = sqrt((k_x - p_j(1))^2 + (k_y - p_j(2))^2);
         i_sum = i_sum + d_i;
@@ -45,12 +41,13 @@ else
     y = p_j - center;
 end
 
-rad = acos(y(2)/norm(y));
+rot = acos(y(2)/norm(y));
 
 if y(1) > 0
-    rot = radtodeg(-rad);
-else
-    rot = radtodeg(rad);
+    rot = -rot;
 end
+
+pos = (center - offset);
+loc = -[cos(-rot) -sin(-rot); sin(-rot) cos(-rot)]*pos';
 
 end
